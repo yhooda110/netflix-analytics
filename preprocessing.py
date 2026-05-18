53e54eb23c16ef9740303f75eac6dd08f1b43903
yhooda110 <yhooda110@gmail.com>
2026-05-18 11:33:23 +0530

    Update preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 def load_data():
-    df = pd.read_csv("netflix_titles.csv")
+    df = pd.read_csv("netflix_titles.csv.zip")
 
     # Fill missing values
     df.fillna("Unknown", inplace=True)
